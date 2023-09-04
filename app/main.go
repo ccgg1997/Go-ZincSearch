d452f4a2f2e5b3964a98dbff8acde97804f6971a
Cristian Gomez <cristian.galvis@correounivalle.edu.co>
2023-09-04 18:45:24 -0500

    ejemplo basico servidor

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
@@ -11,17 +12,29 @@ import (
 )
 
 func main() {
+
 	//iniciar servidor de profiling en un goroutine
 	go func() {
 		log.Println(http.ListenAndServe("0.0.0.0:6060", nil))
 	}()
+
+	//prueba de impresion de uuid
+	fmt.Printf("si estamos en main, Hola mundo, entrada numero: %s \n", uuid.New().String())
+
+	//servidor web principal
+	http.HandleFunc("/miruta", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "mi ruta personalizada")
+	})
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 	fmt.Println(uuid.New().String())
-	fmt.Println("si estamos en main Hola mundo ")
-	fmt.Printf("si estamos en main Hola mundos1234589 %s", uuid.New().String())
 
 	// Forzar a vaciar el búfer de salida
 	os.Stdout.Sync()
+}
 
-	// Bucle infinito para mantener el programa en ejecución
-	select {}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hay peticiones en ejecucion")
+	io.WriteString(w, "Hola mundo")
+
 }
